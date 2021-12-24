e89ddf412548350daf66accc669bf3fead2e8750
User <User@DESKTOP-C7DGT0E>
2021-12-24 13:49:35 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Tashoglo3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите n:");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите х:");
+                double x = double.Parse(Console.ReadLine());
+                double S = 0;
+                for (int i = 0, h=2; i < n; i++, h+=2)
+                {
+                    long f = 1;
+                    for (int j = 1; j <= h; j++) f += j;
+                    {
+                        if (i % 2 == 0) S += -Math.Pow(x - 1, h) / f;
+                        else S += Math.Pow(x - 1, h) / f;
+                    }
+                }
+                Console.WriteLine("S={0:F2}", S);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
+
